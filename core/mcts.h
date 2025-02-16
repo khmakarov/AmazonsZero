74c3be95a7a850005ee71f85a1689a128560d226
khmakarov <12960891+khmakarov@user.noreply.gitee.com>
2025-02-16 13:05:58 +0800

    实现mcts结构

--- core/mcts.h
+++ core/mcts.h
@@ -1,18 +1,40 @@
 #pragma once
-#include <pybind11/stl.h>
 #include "game_core.h"
+#include <vector>
+#include <memory>
 
 class MCTSNode
 {
 public:
-    GameCore state;
-    std::vector<std::tuple<int, int, int>> legal_actions;
-    std::unordered_map<size_t, std::unique_ptr<MCTSNode>> children;
-    float value_sum = 0, prior = 0;
-    int visit_count = 0;
+    MCTSNode(std::shared_ptr<GameCore> state, MCTSNode *parent = nullptr)
+        : state(state), parent(parent), visit_count(0), value_sum(0), prior(0) {}
 
-    MCTSNode(const GameCore &game);
     MCTSNode *select(double c_puct);
     void expand(const std::vector<float> &action_probs);
-    void backup(float value);
+    void update(float value);
+    bool is_leaf() const;
+
+    std::shared_ptr<GameCore> state;
+    MCTSNode *parent;
+    std::vector<std::pair<int, MCTSNode *>> children; // action_index -> node
+    int visit_count;
+    float value_sum;
+    float prior;
+};
+
+class MCTSEngine
+{
+public:
+    MCTSEngine(int n_simulations, double c_puct)
+        : n_simulations(n_simulations), c_puct(c_puct) {}
+
+    std::vector<float> run(std::shared_ptr<GameCore> root_state,
+                           py::function policy_value_fn);
+
+private:
+    void simulate(std::shared_ptr<GameCore> state);
+
+    int n_simulations;
+    double c_puct;
+    std::vector<MCTSNode *> node_pool;
 };
