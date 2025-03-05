326999ad5306c27d8e62982e7c48a8b6aca73d38
khmakarov <12960891+khmakarov@user.noreply.gitee.com>
2025-03-05 19:26:39 +0800

    使用NumPy加速

--- core/python/mcts.py
+++ core/python/mcts.py
@@ -14,88 +14,84 @@ class MCTS():
     def __init__(self, nnet, args):
         self.nnet = nnet
         self.args = args
-        self.Qsa = {}  # stores Q values for s,a (as defined in the paper)
-        self.Nsa = {}  # stores #times edge s,a was visited
-        self.Ns = {}  # stores #times board s was visited
-        self.Ps = {}  # stores initial policy (returned by neural net)
 
-        self.Es = {}  # stores game.getGameEnded ended for board s
-        self.Vs = {}  # stores game.getValidMoves for board s
+        self.Qsa = {}  # 存储每个状态的Q值数组
+        self.Nsa = {}  # 存储每个状态的访问计数数组
+        self.Ns = {}  # 存储状态的访问次数
+        self.Ps = {}  # 存储策略概率数组
+        self.Es = {}  # 存储终止状态
+        self.Vs = {}  # 存储有效动作掩码
 
     def getActionProb(self, game, temp=1):
-
-        for i in range(self.args.numMCTSSims):
+        for _ in range(self.args.numMCTSSims):
             self.search(game)
 
         s = game.stringRepresentation()
         _, valids_idx = game.get_legal_actions()
         n = valids_idx[0]
         counts = np.zeros(TOTAL_ACTIONS, dtype=np.int32)
-        valids_idx = np.array(valids_idx[1:n + 1], dtype=np.int32)
-        counts[valids_idx] = np.fromiter((self.Nsa.get((s, a), 0) for a in valids_idx), dtype=np.int32, count=len(valids_idx))
-
+        if s in self.Nsa:
+            valids = valids_idx[1:n + 1]
+            counts[valids] = self.Nsa[s][valids]
+        else:
+            counts[valids_idx[1:n + 1]] = 0
         if temp == 0:
-            bestAs = np.array(np.argwhere(counts == np.max(counts))).flatten()
-            bestA = np.random.choice(bestAs)
-            probs = [0] * len(counts)
-            probs[bestA] = 1
-            return probs
+            probs = np.zeros_like(counts, dtype=np.float32)
+            bestA = np.argmax(counts)
+            probs[bestA] = 1.0
+        else:
+            probs = np.power(counts, 1.0 / temp)
+            probs = probs / np.sum(probs)
 
-        counts = [x**(1. / temp) for x in counts]
-        counts_sum = float(sum(counts))
-        probs = [x / counts_sum for x in counts]
         return probs
 
     def search(self, game):
-
         s = game.stringRepresentation()
-        valids, valids_idx = game.get_legal_actions()
-        valids = np.array(valids)
+
         if s not in self.Es:
             self.Es[s] = game.is_terminal()
         if self.Es[s] != 0:
             return -self.Es[s]
+
         if s not in self.Ps:
             self.Ps[s], v = self.nnet.predict(np.array(game.get_state()))
-            self.Ps[s] = self.Ps[s] * valids
+            valids, valids_idx = game.get_legal_actions()
+            n = valids_idx[0]
+
+            self.Ps[s] *= valids
             sum_Ps_s = np.sum(self.Ps[s])
             if sum_Ps_s > 0:
-                self.Ps[s] /= sum_Ps_s  # renormalize
+                self.Ps[s] /= sum_Ps_s
             else:
                 log.error("All valid moves were masked, doing a workaround.")
                 self.Ps[s] = self.Ps[s] + valids
                 self.Ps[s] /= np.sum(self.Ps[s])
 
-            self.Vs[s] = valids
+            self.Qsa[s] = np.zeros(TOTAL_ACTIONS, dtype=np.float32)  # 修改点1
+            self.Nsa[s] = np.zeros(TOTAL_ACTIONS, dtype=np.int32)  # 修改点1
+            self.Vs[s] = np.array(valids_idx[1:n + 1], dtype=np.int32)
             self.Ns[s] = 0
             return -v
 
-        valids = self.Vs[s]
-        cur_best = -float('inf')
-        best_act = -1
-        n = valids_idx[0]
-        for a in valids_idx[1:n + 1]:
-            if (s, a) in self.Qsa:
-                u = self.Qsa[(s, a)] + self.args.cpuct * self.Ps[s][a] * math.sqrt(self.Ns[s]) / (1 + self.Nsa[(s, a)])
-            else:
-                u = self.args.cpuct * self.Ps[s][a] * math.sqrt(self.Ns[s] + EPS)  # Q = 0 ?
+        valid_actions = self.Vs[s]
+        if len(valid_actions) == 0:
+            log.error("No valid actions at state %s", s)
+            return 0
+        qs = self.Qsa[s][valid_actions]
+        ps = self.Ps[s][valid_actions]
+        ns = self.Nsa[s][valid_actions]
 
-            if u > cur_best:
-                cur_best = u
-                best_act = a
+        sqrt_Ns = math.sqrt(self.Ns[s])
+        u_values = qs + self.args.cpuct * ps * sqrt_Ns / (1 + ns + EPS)
+        best_index = np.argmax(u_values)
+        best_act = valid_actions[best_index]
 
-        a = best_act
         next_s = GameCore(game)
-        next_s.step(a)
+        next_s.step(best_act)
         v = self.search(next_s)
 
-        if (s, a) in self.Qsa:
-            self.Qsa[(s, a)] = (self.Nsa[(s, a)] * self.Qsa[(s, a)] + v) / (self.Nsa[(s, a)] + 1)
-            self.Nsa[(s, a)] += 1
-
-        else:
-            self.Qsa[(s, a)] = v
-            self.Nsa[(s, a)] = 1
-
+        self.Qsa[s][best_act] = (self.Nsa[s][best_act] * self.Qsa[s][best_act] + v) / (self.Nsa[s][best_act] + 1)
+        self.Nsa[s][best_act] += 1
         self.Ns[s] += 1
+
         return -v
