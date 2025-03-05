59e22e59694e95044a881ff6b8726928690a9421
khmakarov <12960891+khmakarov@user.noreply.gitee.com>
2025-03-05 11:15:46 +0800

    测试对局数据可视化

--- scripts/test_demo.py
+++ scripts/test_demo.py
@@ -1,15 +1,7 @@
 import numpy as np
 from Amazons import GameCore
-
-
-def main():
-    print("a")
-    game = GameCore()
-    print(isinstance(np.array(game.get_state()), np.ndarray))
-    valids, valids_idx = game.get_legal_actions()
-    print(isinstance(valids_idx, np.ndarray))
-    print(isinstance(valids, np.ndarray))
-
+from src.utils.visualize import AmazonsVisualizer
 
 if __name__ == "__main__":
-    main()
+    av = AmazonsVisualizer(2)
+    av.run()
