4515ecbc2e90c4bd1bdfbbee7448e64a157c42ce
khmakarov <khmakarov@user.noreply.gitee.com>
2025-04-13 13:23:11 +0800

    Use lz4, pickle instead of json, zlib serialization

--- src/utils/data_manager.py
+++ src/utils/data_manager.py
@@ -1,7 +1,7 @@
 #data_manager.py
-import json
+import lz4.frame
+import pickle
 import random
-import zlib
 from src.utils.database import AmazonsDatabase
 from core.cpp.build.Amazons import GameCore
 
@@ -10,15 +10,23 @@ class DataManager:
 
     def __init__(self):
         self.db = AmazonsDatabase()
+        self.visual_buffer = []
         self.train_data = []
 
-    def add_episode_data(self, episode_data, result):
-        visual_data = [(DataManager._deserialize_state(s_bytes), a) for s_bytes, a, _ in episode_data]
-        self.db.save_game(visual_data, result)
+    def add_visual_data(self, episode_data, result):
+        visual_data = [(s_bytes, a) for s_bytes, a, _, _ in episode_data]
+        self.visual_buffer.append((visual_data, result))
 
     def add_train_data(self, train_data):
-        processed = [(DataManager._deserialize_state(s_bytes), pi, ended) for s_bytes, pi, ended in train_data]
+        processed = [(DataManager._deserialize_state(s_bytes), pi, valids_idx, ended) for s_bytes, pi, valids_idx, ended in train_data]
         self.train_data.extend(processed)
+
+    def flush_visual_data(self):
+        print("自对弈数据处理完毕")
+        if not self.visual_buffer:
+            return
+        self.db.save_games(self.visual_buffer)
+        self.visual_buffer.clear()
 
     def clear_train_data(self):
         self.train_data.clear()
@@ -29,13 +37,10 @@ class DataManager:
     @staticmethod
     def _deserialize_state(data: bytes):
         """从字节流重建游戏状态"""
-        state_data = json.loads(zlib.decompress(data).decode())
-
-        # 重建GameCore实例
+        state_data = pickle.loads(lz4.frame.decompress(data))
         state = GameCore()
         state.current_player = state_data["current_player"]
         state.black = state_data["black"]
         state.white = state_data["white"]
         state.blocks = state_data["blocks"]
-
         return state
