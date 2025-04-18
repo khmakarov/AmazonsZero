444f467cc566517a1be7eb92e3eb1a6413a67950
khmakarov <khmakarov@user.noreply.gitee.com>
2025-04-18 19:41:36 +0800

    Optimize data pipeline and streamline training process logging

--- src/train/train.py
+++ src/train/train.py
@@ -8,29 +8,26 @@ from torch.optim import Adam
 from core.cpp.build.Amazons import GameCore
 from core.python.mcts import MCTS
 from core.python.net import AlphaZeroNet
-from evaluate import Evaluator
 from src.utils.ckpt_manager import CheckpointManager
 from src.utils.data_manager import DataManager
+from evaluate import Evaluator
 from torch.utils.tensorboard import SummaryWriter
 
 
 class Trainer:
 
     def __init__(self, cfg):
-        # 初始化基础组件
         self.device = torch.device("cuda")
         self.load_model = cfg.load_model
         self.mcts = cfg.mcts
         self.nnet = self._init_net()
         self.training = cfg.training
         self.win_rate = cfg.evaluator.win_rate
-        # 模块化组件
         self.optimizer = Adam(self.nnet.parameters(), lr=cfg.training.lr, weight_decay=cfg.training.weight_decay)
         self.ckpt_mgr = CheckpointManager()
         self.data_mgr = DataManager()
         self.evaluator = Evaluator(cfg)
         self.writer = SummaryWriter(cfg.log_dir)
-
         self.iteration = 0
         self.writer.add_graph(self.nnet, torch.randn(1, 5, 8, 8).to(self.device))
 
@@ -56,30 +53,30 @@ class Trainer:
     def _generate_episodes(self):
         nnet_state_dict = self.nnet.to("cpu").state_dict()
         self.nnet.to(self.device)
-        with mp.Pool(processes=os.cpu_count()) as pool:
+        with mp.Pool(processes=8) as pool:
             tasks = [(nnet_state_dict, self.mcts)] * self.training.num_eps
             results = pool.starmap(self.execute_episode_worker, tasks)
             pool.close()
             pool.join()
+        print("自对弈数据生成完毕")
         return results
 
     def _process_episodes(self, results):
-        print("自对弈数据生成完毕")
-        for train_data, visual_data, result in results:
+        for episode_data, ended in results:
+            train_data, visual_data = zip(*[((x[0], x[1], x[2], ended), (x[0], x[3])) for x in episode_data])
             self.data_mgr.add_train_data(train_data)
-            self.data_mgr.add_visual_data(visual_data, result)
+            self.data_mgr.add_visual_data(visual_data, ended, self.iteration, 0)
+        print("该迭代数据保存完毕")
 
     def _train_step(self):
-        print("该迭代数据保存完毕")
         batch = self.data_mgr.sample_batch(self.training.batch_size)
         total_loss, loss_pi, loss_v = self.train(batch)
-
+        print("该迭代数据训练完毕")
         self.writer.add_scalar('Loss/total', total_loss, self.iteration)
         self.writer.add_scalar('Loss/policy', loss_pi, self.iteration)
         self.writer.add_scalar('Loss/value', loss_v, self.iteration)
 
     def _evaluation_step(self):
-        print("该迭代数据训练完毕")
         latest_path = self.ckpt_mgr.get_latest_checkpoint()
         current_path = self.ckpt_mgr.save(self.nnet, self.optimizer)
 
@@ -90,8 +87,8 @@ class Trainer:
             del self.nnet
             torch.cuda.empty_cache()
 
-        win_rate = self.evaluator.evaluate(current_path, latest_path)
-        self.nnet = AlphaZeroNet().to(self.device)
+        win_rate = self.evaluator.evaluate(self.iteration, current_path, latest_path)
+        self.nnet = self._init_net()
 
         if win_rate >= self.win_rate:
             checkpoint = torch.load(current_path)
@@ -129,45 +126,45 @@ class Trainer:
         total_loss.backward()
         self.optimizer.step()
         torch.cuda.empty_cache()
-        if self.iteration % 10 == 0:
-            for name, param in self.nnet.named_parameters():
-                self.writer.add_histogram(f'Parameters/{name}', param, self.iteration)
-                if param.grad is not None:
-                    self.writer.add_histogram(f'Gradients/{name}', param.grad, self.iteration)
+        for name, param in self.nnet.named_parameters():
+            self.writer.add_histogram(f'Parameters/{name}', param, self.iteration)
+            if param.grad is not None:
+                self.writer.add_histogram(f'Gradients/{name}', param.grad, self.iteration)
 
         return total_loss.item(), loss_pi.item(), loss_v.item()
 
     @staticmethod
     def execute_episode_worker(nnet_state_dict, cfg):
         try:
-            # 子进程初始化模型和MCTS
-            device = torch.device("cuda")
-            nnet = AlphaZeroNet().to(device)
+            episode_data = []
+            nnet = AlphaZeroNet().to("cuda")
             nnet.load_state_dict(nnet_state_dict)
             mcts = MCTS(nnet, cfg)
-            episode_data = []
             state = GameCore()
             while True:
-                pi, valids_idx = mcts.getActionProb(state)
-                action_index = np.random.choice(len(pi), p=pi)
-                next_state = GameCore(state)
-                next_state.step(action_index)
-                ended = next_state.is_terminal()
-                action = state.index2action(action_index)
-                episode_data.append([state.get_state_np(), action, pi, valids_idx])
-                if ended != 0:
-                    result = "黑胜" if (ended == 1 and next_state.current_player == 0) or (ended == 0 and next_state.current_player == 1) else "白胜"
+                ended = state.is_terminal()
+                if ended == 0:
+                    pi, valids_idx = mcts.getActionProb(state)
+                    action_index = np.random.choice(len(pi), p=pi)
+                    next_state = GameCore(state)
+                    next_state.step(action_index)
+                    episode_data.append([state.get_state_np(), pi, valids_idx, state.index2action(action_index)])
+                    state = next_state
+                else:
                     print(" 对局结束")
+                    episode_data.append(
+                        [
+                            state.get_state_np(),
+                            np.zeros(cfg.TOTAL_ACTIONS, dtype=np.float64),
+                            np.zeros(cfg.POSSIBLE_ACTIONS, dtype=np.int32),
+                            state.index2action(0)
+                        ]
+                    )
                     break
-                state = next_state
-            return (
-                [(x[0], x[2], x[3], ended) for x in episode_data],  # 训练样本
-                episode_data + [[next_state.get_state_np(), None, None, None]],  # 回放样本
-                result
-            )
+            return (episode_data, ended)
         finally:
             print(" 子进程返回")
-            del mcts, nnet, state, next_state
+            del mcts, nnet
             torch.cuda.empty_cache()
 
     def __del__(self):
