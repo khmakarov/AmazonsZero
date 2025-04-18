2baefc841af2b35b2e88c15eafaefdf5180c99d1
khmakarov <khmakarov@user.noreply.gitee.com>
2025-04-18 19:33:55 +0800

    Refactor: Precompute composite action key for readability

--- core/cpp/action_index.cpp
+++ core/cpp/action_index.cpp
@@ -27,7 +27,8 @@ py::array_t<int> generate_mask_np(const std::pair<std::array<MoveAction, POSSIBL
 	{
 		if (count == counts)
 			break;
-		if (auto it = action_map.find(static_cast<size_t>(std::get<0>(action)) << 16 | (static_cast<size_t>(std::get<1>(action)) << 8) | static_cast<size_t>(std::get<2>(action))); it != action_map.end())
+		auto action_sum = static_cast<size_t>(std::get<0>(action)) << 16 | (static_cast<size_t>(std::get<1>(action)) << 8) | static_cast<size_t>(std::get<2>(action));
+		if (auto it = action_map.find(action_sum); it != action_map.end())
 			buf[++count] = static_cast<int>(it->second);
 	}
 	return mask_index;
