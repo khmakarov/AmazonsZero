df29574a62e52075cd21fe48262b28743a685f5a
khmakarov <khmakarov@user.noreply.gitee.com>
2025-04-22 23:36:48 +0800

    Optimize MCTS with batch child state prediction and memory management

--- core/python/mcts.py
+++ core/python/mcts.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import torch
 from core.cpp.build.Amazons import GameCore
 
 
@@ -18,71 +19,62 @@ class MCTS():
         self.Vs = {}  # 存储有效动作掩码
 
     def getActionProb(self, game, temp=1):
-        for _ in range(self.num_simulations):
-            self.search(game)
-
-        s = game.compute_state_hash()
-        valids_idx = game.get_legal_actions_np()
-        counts = np.zeros(self.total_actions, dtype=np.int32)
-
-        if s in self.Nsa:
-            counts[self.Vs[s]] = self.Nsa[s][self.Vs[s]]
-        else:
-            counts[valids_idx[1:valids_idx[0] + 1]] = 0
-
-        if temp == 0:
-            probs = np.zeros_like(counts, dtype=np.float32)
-            bestA = np.argmax(counts)
-            probs[bestA] = 1.0
-        else:
+        try:
+            s = game.compute_state_hash()
+            valids_idx = game.get_legal_actions_np()
+            s_hashes, s_states, s_valids = game.get_child_state_np(valids_idx)
+            pis, _ = self.nnet.predict_batch(s_states, s_valids)
+            for i in range(valids_idx[0]):
+                child_hash = s_hashes[i]
+                if child_hash not in self.Psa:
+                    valid_actions = s_valids[i][1:s_valids[i][0] + 1]
+                    self.Psa[child_hash] = pis[i][valid_actions] / np.sum(pis[i][valid_actions])  # 归一化
+                    self.Qsa[child_hash] = np.zeros(s_valids[i][0], dtype=np.float32)
+                    self.Nsa[child_hash] = np.zeros(s_valids[i][0], dtype=np.int32)
+                    self.Vs[child_hash] = valid_actions
+                    self.Ns[child_hash] = 1
+            del pis, _
+            for _ in range(self.num_simulations):
+                self.search(game)
+            counts = np.zeros(self.total_actions, dtype=np.int32)
+            counts[self.Vs[s]] = self.Nsa[s]
             probs = np.power(counts, 1.0 / temp)
             probs = probs / np.sum(probs)
-
-        self.clear()
-        return probs, valids_idx
+            return probs, valids_idx
+        finally:
+            torch.cuda.empty_cache()
+            self.clear()
 
     def search(self, game):
-        s = game.compute_state_hash()
-
-        if s not in self.Psa:
-            valids_idx = game.get_legal_actions_np()
-            valids = np.zeros(self.total_actions, dtype=np.bool_)
-            valids[valids_idx[1:valids_idx[0] + 1]] = True
-            self.Psa[s], v = self.nnet.predict(game, valids_idx)
-            sum_Psa_s = np.sum(self.Psa[s])
-            if sum_Psa_s > 0:
-                self.Psa[s] /= sum_Psa_s
+        try:
+            s = game.compute_state_hash()
+            if s not in self.Psa:
+                valids_idx = game.get_legal_actions_np()
+                pi, v = self.nnet.predict(game.get_state_np(), valids_idx)
+                valid_actions = valids_idx[1:valids_idx[0] + 1]
+                self.Psa[s] = pi[valid_actions] / np.sum(pi[valid_actions])
+                self.Qsa[s] = np.zeros(valids_idx[0], dtype=np.float32)
+                self.Nsa[s] = np.zeros(valids_idx[0], dtype=np.int32)
+                self.Vs[s] = valid_actions
+                self.Ns[s] = 1
+                return v * (1 - 2 * game.current_player)
+            if self.Vs[s].size != 0:
+                valid_actions = self.Vs[s]
+                sqrt_Ns = math.sqrt(self.Ns[s])
+                u_values = self.Qsa[s] + self.cpuct * self.Psa[s] * sqrt_Ns / (1 + self.Nsa[s])
+                best_index = np.argmax(u_values)
+                best_act = valid_actions[best_index]
+                next_s = GameCore(game)
+                next_s.step(best_act)
+                v = self.search(next_s)
+                self.Qsa[s][best_index] = (self.Nsa[s][best_index] * self.Qsa[s][best_index] + v) / (self.Nsa[s][best_index] + 1)
+                self.Nsa[s][best_index] += 1
+                self.Ns[s] += 1
+                return -v
             else:
-                self.Psa[s] = self.Psa[s] + valids
-                self.Psa[s] /= np.sum(self.Psa[s])
-
-            self.Qsa[s] = np.zeros(self.total_actions, dtype=np.float32)
-            self.Nsa[s] = np.zeros(self.total_actions, dtype=np.int32)
-            self.Vs[s] = valids_idx[1:valids_idx[0] + 1]
-            self.Ns[s] = 1
-            return v * (1 - 2 * game.current_player)
-        if self.Vs[s].size != 0:
-            valid_actions = self.Vs[s]
-            qsa = self.Qsa[s][valid_actions]
-            psa = self.Psa[s][valid_actions]
-            nsa = self.Nsa[s][valid_actions]
-
-            sqrt_Ns = math.sqrt(self.Ns[s])
-            u_values = qsa + self.cpuct * psa * sqrt_Ns / (1 + nsa)
-            best_index = np.argmax(u_values)
-            best_act = valid_actions[best_index]
-
-            next_s = GameCore(game)
-            next_s.step(best_act)
-            v = self.search(next_s)
-
-            self.Qsa[s][best_act] = (self.Nsa[s][best_act] * self.Qsa[s][best_act] + v) / (self.Nsa[s][best_act] + 1)
-            self.Nsa[s][best_act] += 1
-            self.Ns[s] += 1
-
-            return -v
-        else:
-            return 1.0 if game.current_player == 1 else -1.0
+                return 1.0 if game.current_player == 1 else -1.0
+        finally:
+            torch.cuda.empty_cache()
 
     def clear(self):
         self.Nsa.clear()
