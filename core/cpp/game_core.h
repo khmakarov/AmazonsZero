e58b67041ff6e44b28f0e7645542cbaf36dcbf20
khmakarov <khmakarov@user.noreply.gitee.com>
2025-04-22 23:27:26 +0800

    Optimize move generation with precomputed masks and parallel processing

--- core/cpp/game_core.h
+++ core/cpp/game_core.h
@@ -1,7 +1,24 @@
 #pragma once
-#include <xxhash.h>
+#include <array>
+#include <cstdint>
+#include <cstdlib>
+#include <fstream>
 #include <functional>
-#include "action_index.h"
+#include <immintrin.h>
+#include <omp.h>
+#include <pybind11/stl.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+#include <tuple>
+#include <unordered_map>
+#include <xxhash.h>
+namespace py = pybind11;
+using MoveAction = std::tuple<uint8_t, uint8_t, uint8_t>;
+using ChildState = std::tuple<py::array_t<size_t>, py::array_t<int8_t>, py::array_t<int>>;
+constexpr size_t TOTAL_ACTIONS = 33344, POSSIBLE_ACTIONS = 1500;
+inline std::array<MoveAction, TOTAL_ACTIONS> action_list;
+inline std::unordered_map<size_t, size_t> action_map;
+void load_actions();
 class GameCore
 {
 public:
@@ -11,10 +28,11 @@ public:
 	int current_player;
 
 	size_t compute_state_hash() const;
-	py::array_t<int8_t> get_state_np() const; // 将对局状态输入python端
-	py::array_t<int> get_legal_actions_np();  // 返回当前棋盘状态所有合法动作的掩码
-	void step(int action_index);			  // 应用行动
-	int is_terminal();						  // 0未结束,1 黑方赢,-1 白方赢
+	py::array_t<int8_t> get_state_np() const;					// 将对局状态输入python端
+	py::array_t<int> get_legal_actions_np();					// 返回当前棋盘状态所有合法动作的掩码
+	ChildState get_child_state_np(py::array_t<int> valids_idx); // 所有子状态的np数组和合法掩码
+	void step(int action_index);								// 应用行动
+	int is_terminal();											// 0未结束,1 黑方赢,-1 白方赢
 	MoveAction index2action(int index);
 
 private:
@@ -28,14 +46,21 @@ private:
 		{0x00FEFEFEFEFEFEFE, -7}, // NorthEast
 		{0x7F7F7F7F7F7F7F00, 7},  // SouthWest
 	}};
+	alignas(32) static constexpr std::array<uint64_t, 64> MASK_TABLE = []()
+	{
+		std::array<uint64_t, 64> masks;
+		for (int i = 0; i < 64; ++i)
+			masks[i] = 1ULL << i;
+		return masks;
+	}();
 	uint64_t black, white, blocks;
 	uint64_t piece_from_backpack = 0, piece_to_backpack = 0;
 
 	uint64_t generate_moves(uint64_t from) const;
 	static uint64_t ray_cast(uint64_t from, std::pair<uint64_t, int> dir_mask, uint64_t blanks);
+	void fill_legal_actions(int *target, int &count);
 	void apply_move(uint64_t from, uint64_t to);
 	void restore_action();
-	static MoveAction pack_action(uint64_t from, uint64_t to, uint64_t block);
 	static std::array<uint64_t, 3> unpack_action(const std::tuple<int, int, int> &action);
 	static std::array<uint64_t, 4> unpack_pieces(uint64_t pieces);
 	static uint64_t lowest1(uint64_t x);
